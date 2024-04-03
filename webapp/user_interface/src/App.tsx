deda7d80d02adf91ef5ef6b4576f5f5d9a674172
Netanel Kuchma <nati190819@icloud.com>
2024-04-03 09:59:27 +0300

    UI menu is ready

--- webapp/user_interface/src/App.tsx
+++ webapp/user_interface/src/App.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar/NavBar';
+ import Home from './components/Homepage/Homepage';
+// import About from './About';
+// import Services from './Services';
+// import Contact from './Contact';
 const App: React.FC = ()=> {
   return (
     <div className="App">
-      <div className="FirstRow">
-        <div className="menu"> 
-        <div className="menu1"></div>
-        <div className="menu1"></div>
-        {/* <div className="menu1"></div>
-        <div className="menu1"></div>
-        <div className="menu1"></div>
-        <div className="menu1"></div> */}
-        </div>
-      </div>
-     
-     <div className="SecondRow">bbb</div>
-     <div className="ThirdRow">
-      <div className="rectangle">
-        <div className='rec1'>Image </div>
-        
-        <div className='rec2'>text </div>
-      </div>
-      <div className="rectangle"></div>
-      <div className="rectangle"></div>
+      <div className='column1'></div>
+      <div className='column2'>
+      <Router>
+      
+      <NavBar />
+          <Routes>
+          <Route path="/" element={<Home/>} />
+          {/* <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} /> */}
+          </Routes>
+
+    </Router>
      </div>
-    </div>
+     </div>
   );
 }
 
---
+++ webapp/user_interface/src/components/Homepage/Homepage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Homepage.css'
+const Homepage: React.FC = () => {
+    
+
+
+  return (
+    <div className='mainWindow'> 
+        <div className="SecondRow">bbb</div>
+        <div className="ThirdRow">
+     <div className="rectangle">
+        <div className='rec1'>Image </div>
+        
+        <div className='rec2'>text </div>
+      </div>
+      <div className="rectangle"></div>
+      <div className="rectangle"></div> 
+    </div>
+    </div>
+  );
+}
+
+export default Homepage;
